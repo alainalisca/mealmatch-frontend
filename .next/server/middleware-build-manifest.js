c8ad0b6f618746512237ef0898eee1d20ceb7bd2
alainalisca <98769249+alainalisca@users.noreply.github.com>
2025-09-28 14:49:09 -0400

    Fix build script and gitignore

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -20,15 +20,10 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/orders": [
+    "/cooks": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/orders.js"
-    ],
-    "/recipes/[id]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/recipes/[id].js"
+      "static/chunks/pages/cooks.js"
     ]
   },
   "ampFirstPages": []
